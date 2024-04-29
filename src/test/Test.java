60e7227f9926c75854a362be9eff58b2df0af16f
altih <altihnic@gmail.com>
2024-04-29 17:51:02 +0400

    Init

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,5 +8,11 @@ public class Test {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         Collections.addAll(list,1,2,3,4,5,6);
+        System.out.println();
+       for (Integer i: list)
+           System.out.println(i);
+
+        }
+
     }
-}
+
