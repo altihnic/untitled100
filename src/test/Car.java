ee61f62b9e766f7167acabd1112dd6bf378dd4ba
altih <altihnic@gmail.com>
2024-04-29 18:11:42 +0400

    Init

--- src/test/Car.java
+++ src/test/Car.java
@@ -1,4 +1,9 @@
 package test;
 
+import java.time.LocalDate;
+
 public class Car {
+    private String name;
+    private String color;
+    private LocalDate createDate;
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -7,13 +7,14 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        Collections.addAll(list,1,2,3,4,5,6);
+        Collections.addAll(list, 1, 2, 3, 4, 5, 6);
         System.out.println();
-       for (Integer i: list)
-           System.out.println(i);
-        System.out.println();
+        for (Integer i : list) {
 
         }
 
+
     }
+
+}
 
