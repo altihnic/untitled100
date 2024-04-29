d5114e176fdc19391b3cfc4ff4bcaf4aee99ba88
altih <altihnic@gmail.com>
2024-04-29 16:21:53 +0400

    Init

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,2 +1,12 @@
-package test;public class Test {
+package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list,1,2,3,4,5,6);
+    }
 }
