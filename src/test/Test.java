036cbd49e6d99185da2fc53cd7171ded5c4773d7
altih <altihnic@gmail.com>
2024-05-02 17:34:11 +0400

    init

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,9 +3,7 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("Здорово");
 
     }
 }
